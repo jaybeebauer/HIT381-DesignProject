3c3fdb7343f2ce051d40757cc131c95369af3639
Joshua Bauer <joshybee@gmail.com>
2018-04-21 13:25:24 +0800

    -Removed test traillist and set into index.html

--- Week7/js/lib/traillistxml.js
+++ Week7/js/lib/traillistxml.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 
     //but if it's multible items then loop
     $(xml).find('trail').each(function(){
-      $("#traillist" ).append('<a href="#Trail'+$(this).find('Id').text()+'" data-role="button"><li><img src="'+$(this).find('Image').text()+'"/><h3>'+$(this).find('Name').text()+'</h3><p>'+$(this).find('Notes').text()+'</p></li></a>');
+      $("#traillist" ).append('<a href="#Trail'+$(this).find('Id').text()+'" data-role="button"><li><img src="'+$(this).find('Image').text()+'"/><h3>'+$(this).find('Name').text()+'</h3><p>'+$(this).find('Description').text()+'</p></li></a>');
     });
     }
 });
